6f21a229a7bd0a8f376df6f9e7766171dd41b728
Anjo <350anviera195@gmail.com>
2019-04-28 18:10:13 +0800

    Updates

---
+++ precache-manifest.ef705336b26add9f8d547635113a16b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cb9efa24e73f044f2ab",
+    "url": "/testcm/static/js/runtime~main.05bc237c.js"
+  },
+  {
+    "revision": "aac36a65ba14cf8c41ae",
+    "url": "/testcm/static/js/main.e2730dd1.chunk.js"
+  },
+  {
+    "revision": "663a69b6264ddb1e1fe9",
+    "url": "/testcm/static/js/2.1ab9c3b5.chunk.js"
+  },
+  {
+    "revision": "aac36a65ba14cf8c41ae",
+    "url": "/testcm/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "663a69b6264ddb1e1fe9",
+    "url": "/testcm/static/css/2.1b6e13a5.chunk.css"
+  },
+  {
+    "revision": "2c6fb8d9633f5b6872dfaf1d2c77f09b",
+    "url": "/testcm/index.html"
+  }
+];
